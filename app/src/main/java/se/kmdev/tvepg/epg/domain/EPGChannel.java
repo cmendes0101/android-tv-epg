b11250c6350937dc1611a6257da3beec67f5df38
Chris Mendes <cmendes0101@gmail.com>
2016-08-27 18:18:46 -0700

    Expanded classes for more data

--- app/src/main/java/se/kmdev/tvepg/epg/domain/EPGChannel.java
+++ app/src/main/java/se/kmdev/tvepg/epg/domain/EPGChannel.java
@@ -5,25 +5,40 @@ package se.kmdev.tvepg.epg.domain;
  */
 public class EPGChannel {
 
-    private final String channelID;
-    private final String name;
-    private final String imageURL;
-
-    public EPGChannel(String imageURL, String name, String channelID) {
-        this.imageURL = imageURL;
-        this.name = name;
-        this.channelID = channelID;
-    }
+    private String channelID;
+    private String name;
+    private String imageURL;
+    private String language;
 
     public String getChannelID() {
         return channelID;
+    }
+
+    public void setChannelID(String channelID) {
+        this. channelID = channelID;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getImageURL() {
         return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
     }
 }
--- app/src/main/java/se/kmdev/tvepg/epg/domain/EPGEvent.java
+++ app/src/main/java/se/kmdev/tvepg/epg/domain/EPGEvent.java
@@ -5,30 +5,74 @@ package se.kmdev.tvepg.epg.domain;
  */
 public class EPGEvent {
 
-    private final long start;
-    private final long end;
-    private final String title;
-
-    public EPGEvent(long start, long end, String title) {
-        this.start = start;
-        this.end = end;
-        this.title = title;
-    }
+    private long start;
+    private long end;
+    private String title;
+    private String imageURL;
+    private String description;
+    private int episodeSeason;
+    private int episodeNumber;
 
     public long getStart() {
         return start;
+    }
+
+    public void setStart(Long start) {
+        this.start = start;
     }
 
     public long getEnd() {
         return end;
     }
 
+    public void setEnd(Long end) {
+        this.end = end;
+    }
+
     public String getTitle() {
         return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public boolean isCurrent() {
         long now = System.currentTimeMillis();
         return now >= start && now <= end;
     }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getEpisodeSeason() {
+        return episodeSeason;
+    }
+
+    public void setEpisodeSeason(int episodeSeason) {
+        this.episodeSeason = episodeSeason;
+    }
+
+    public int getEpisodeNumber() {
+        return episodeNumber;
+    }
+
+    public void setEpisodeNumber(int episodeNumber) {
+        this.episodeNumber = episodeNumber;
+    }
+
+
 }
